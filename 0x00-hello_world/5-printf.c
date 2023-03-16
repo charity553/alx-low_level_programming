33da1ecb50d55f2cf1c149b1c61c4e7ca78ca9a5
charity553 <kokicharity3@gmail.com>
2023-03-16 04:49:48 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
+*Use the function printf
+*You are not allowed to use the function puts
+*Your program should return 0
+*Your program should compile without warning when using the -Wall gcc option
+*/
+int main(void) {
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);}
+		
